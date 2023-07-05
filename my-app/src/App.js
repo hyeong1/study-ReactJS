99314771c06cdcf81c340b4fa95668d53409700c
hyeong1 <mi75625@gmail.com>
2023-07-05 22:57:19 +0900

    useEffect

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,11 +1,32 @@
-import Button from "./Button";
-import styles from "./App.module.css";
+import { useState, useEffect } from "react";
 
 function App() {
+  const [counter, setValue] = useState(0);
+  const [keyword, setKeyword] = useState("");
+  const onClick = () => setValue((prev) => prev + 1);
+  const onChange = (event) => setKeyword(event.target.value);
+  console.log("i run all the time");
+
+  useEffect(() => {
+    console.log("I run only once");
+  }, []); // 딱 한 번만 실행시키고 싶은 코드가 있을 때 useEffect 사용
+  useEffect(() => {
+    console.log("I run when 'keyword' changes");
+  }, [keyword]); // keyword가 변할 때만 실행할 것이라고 알려줌
+  useEffect(() => {
+    console.log("I run when 'counter' changes");
+  }, [counter]);
+
   return (
     <div>
-      <h1 className={styles.title}>Welcome back!!!</h1>
-      <Button text={"Continue"} />
+      <input
+        value={keyword}
+        onChange={onChange}
+        type="text"
+        placeholder="Search here..."
+      />
+      <h1>{counter}</h1>
+      <button onClick={onClick}>Click me</button>
     </div>
   );
 }
