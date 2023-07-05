1a4e0a30688d2761c4042315379bbef95a5d5b12
hyeong1 <mi75625@gmail.com>
2023-07-05 23:59:49 +0900

    to do list

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,25 +1,54 @@
-import { useState, useEffect } from "react";
-
-function Hello() {
-  // clean up function
-  useEffect(() => {
-    console.log("hi");
-    return () => {
-      console.log("bye");
-    };
-  }, []);
-
-  return <h1>Hello</h1>;
-}
+import { useState } from "react";
 
 function App() {
-  const [showing, setShowing] = useState(false);
-  const onClick = () => setShowing((prev) => !prev);
+  const [toDo, setToDo] = useState("");
+  const [toDos, setToDos] = useState([]);
+  const onChange = (event) => setToDo(event.target.value);
+  const onSubmit = (event) => {
+    event.preventDefault();
+    console.log(toDo);
+    if (toDo === "") {
+      return;
+    }
+    setToDos((currentArray) => [toDo, ...currentArray]); // currentArray에 새로운 toDo 추가
+    setToDo(""); // submit 하면 input 비우기
+  };
+  // todo 삭제
+  const deleteToDo = (index) => {
+    setToDos(toDos.filter((item, toDoIndex) => index !== toDoIndex)); // index 일치하지 않는 todo만 남김(index 일치하는 todo 삭제)
+  };
 
   return (
     <div>
-      {showing ? <Hello /> : null}
-      <button onClick={onClick}>{showing ? "Hide" : "Show"}</button>
+      <h1>My To Dos ({toDos.length})</h1>
+      <form onSubmit={onSubmit}>
+        <input
+          onChange={onChange}
+          value={toDo}
+          type="text"
+          placeholder="Write your to do."
+        />
+        <button>Add To Do</button>
+      </form>
+      <hr />
+      <ul>
+        {toDos.map((item, index) => (
+          <li key={index}>
+            {item}
+            <button
+              style={{
+                marginLeft: "5px",
+                border: "none",
+                color: "white",
+                backgroundColor: "tomato",
+              }}
+              onClick={() => deleteToDo(index)} // onClick함수는 deleteToDo를 호출하는데 삭제할 todo의 index와 함께 호출함
+            >
+              x
+            </button>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
