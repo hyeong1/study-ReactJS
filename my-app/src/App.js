75c9d76679ccad0d07b196c7bb820f1fe8143079
hyeong1 <mi75625@gmail.com>
2023-07-06 23:43:44 +0900

    Coin Tracker -dollar to coin

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,54 +1,63 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 function App() {
-  const [toDo, setToDo] = useState("");
-  const [toDos, setToDos] = useState([]);
-  const onChange = (event) => setToDo(event.target.value);
-  const onSubmit = (event) => {
-    event.preventDefault();
-    console.log(toDo);
-    if (toDo === "") {
-      return;
-    }
-    setToDos((currentArray) => [toDo, ...currentArray]); // currentArray에 새로운 toDo 추가
-    setToDo(""); // submit 하면 input 비우기
-  };
-  // todo 삭제
-  const deleteToDo = (index) => {
-    setToDos(toDos.filter((item, toDoIndex) => index !== toDoIndex)); // index 일치하지 않는 todo만 남김(index 일치하는 todo 삭제)
-  };
+  const [loading, setLoading] = useState(true);
+  const [coins, setCoins] = useState([]);
+  const [dollar, setDollar] = useState(0); // 사용자가 입력하는 달러
+  const [cost, setCost] = useState(0); // 선택한 코인 가격
+  useEffect(() => {
+    fetch("https://api.coinpaprika.com/v1/tickers")
+      .then((response) => response.json())
+      .then((json) => {
+        setCoins(json);
+        setLoading(false);
+      });
+  }, []);
+
+  const onChange = (event) => setDollar(event.target.value); // dollar 세팅
+  const onSelect = (event) => {
+    setCost(event.target.value);
+  }; // cost 세팅
 
   return (
     <div>
-      <h1>My To Dos ({toDos.length})</h1>
-      <form onSubmit={onSubmit}>
+      <h1>The Coins! {loading ? "" : coins.length}</h1>
+      {loading ? (
+        <strong>Loading...</strong>
+      ) : (
+        <select onChange={onSelect}>
+          {coins.map((coin) => (
+            <option key={coin.id} value={coin.quotes.USD.price}>
+              {coin.name} ({coin.symbol}): ${coin.quotes.USD.price}USD
+            </option>
+          ))}
+        </select>
+      )}
+      <form>
         <input
+          type="number"
+          placeholder="input dollar"
           onChange={onChange}
-          value={toDo}
-          type="text"
-          placeholder="Write your to do."
+          value={dollar}
         />
-        <button>Add To Do</button>
+        {cost === 0 ? (
+          <span
+            style={{
+              marginLeft: "5px",
+            }}
+          >
+            Chose the coin
+          </span>
+        ) : (
+          <span
+            style={{
+              marginLeft: "5px",
+            }}
+          >
+            You can get {dollar / cost}
+          </span>
+        )}
       </form>
-      <hr />
-      <ul>
-        {toDos.map((item, index) => (
-          <li key={index}>
-            {item}
-            <button
-              style={{
-                marginLeft: "5px",
-                border: "none",
-                color: "white",
-                backgroundColor: "tomato",
-              }}
-              onClick={() => deleteToDo(index)} // onClick함수는 deleteToDo를 호출하는데 삭제할 todo의 index와 함께 호출함
-            >
-              x
-            </button>
-          </li>
-        ))}
-      </ul>
     </div>
   );
 }
